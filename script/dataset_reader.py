9c05350c15bab915932a10515d92ed43a1b3b6fd
idlesilver <512845847@qq.com>
2020-07-27 12:05:58 +0800

    脚本路径统一为工作区

--- script/dataset_reader.py
+++ script/dataset_reader.py
@@ -1,20 +1,20 @@
 # %%
 import pandas as pd
 import matplotlib.pyplot as plt
-import os,sys
-os.chdir(sys.path[0]) # 把运行目录强制转移到当前脚本所在文件夹
-print(f"工作路径切换到当前脚本路径: {os.getcwd()}")
+# import os,sys
+# os.chdir(sys.path[0]) # 把运行目录强制转移到当前脚本所在文件夹
+# print(f"工作路径切换到当前脚本路径: {os.getcwd()}")
 
 # %%
-csv1 = pd.read_csv('./../dataset/raw_dataset/trainset/train_op.csv')
+csv1 = pd.read_csv('./dataset/raw_dataset/trainset/train_op.csv')
 
 
 # %%
 print(csv1[0:1]['user'].values)
 
 # %%
-trans = pd.read_csv(r"./../dataset/raw_dataset/trainset/train_trans.csv")
-sample_trans = pd.read_csv(r"./../dataset/raw_dataset/sample_trainset/sample_trans.csv")
+trans = pd.read_csv(r"./dataset/raw_dataset/trainset/train_trans.csv")
+sample_trans = pd.read_csv(r"./dataset/raw_dataset/sample_trainset/sample_trans.csv")
 
 print(sample_trans.head(10))
 
