da8c22712cfa6ee23e1280bc2ab7c45c713580a7
RickyL-2000 <15305813731@163.com>
2020-07-28 16:42:21 +0800

    补充省市的onehot+降维和user的数值化

--- dataset/dataset1/missing_value_process.py
+++ dataset/dataset1/missing_value_process.py
@@ -4,6 +4,8 @@ import numpy as np
 import os,sys
 import re
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.decomposition import PCA
 
 #%% initial path
 cur_path = sys.path[0].split(os.path.sep)
@@ -78,5 +80,81 @@ if __name__ == "__main__":
             base2.to_csv(f,index=False)
 
 
+# %%
+def process_base_onehot(base_dir, dim):
+    train_df = pd.read_csv(base_dir + '/dataset/dataset1/trainset/train_base.csv')
+    test_df = pd.read_csv(base_dir + '/dataset/dataset1/testset/test_a_base.csv')
+    province = pd.concat([train_df['province'], test_df['province']])
+    city = pd.concat([train_df['city'], test_df['city']])
+
+    values_pro = province.unique().tolist()
+    m_pro = dict(zip(values_pro, range(len(values_pro))))
+    train_df['province'] = train_df['province'].map(lambda x: m_pro[x])
+    train_df = train_df.join(pd.get_dummies(train_df['province']))
+    train_df = train_df.drop(labels='province', axis=1)
+
+    test_df['province'] = test_df['province'].map(lambda x: m_pro[x])
+    test_df = test_df.join(pd.get_dummies((test_df['province'])))
+    test_df = test_df.drop(labels='province', axis=1)
+
+    values_ct_org = city.unique().tolist()
+    values_ct = np.array(values_ct_org).reshape(len(values_ct_org), -1)
+    enc = OneHotEncoder()
+    enc.fit(values_ct)
+    onehot = enc.transform(values_ct).toarray()
+
+    pca = PCA(n_components=dim)
+    pca.fit(onehot)
+    result = pca.transform(onehot)
+    mp = dict(zip(values_ct_org, [code for code in result]))
+
+    newdf_train = pd.DataFrame(columns=['city_'+str(i) for i in range(dim)])
+    for i in range(len(train_df)):
+        code = mp[train_df.loc[i, 'city']]
+        newdf_train.loc[len(newdf_train)] = code
+    train_df = train_df.join(newdf_train)
+    train_df = train_df.drop(labels='city', axis=1)
+
+    newdf_test = pd.DataFrame(columns=['city_'+str(i) for i in range(dim)])
+    for i in range(len(test_df)):
+        code = mp[test_df.loc[i, 'city']]
+        newdf_test.loc[len(newdf_test)] = code
+    test_df = test_df.join(newdf_test)
+    test_df = test_df.drop(labels='city', axis=1)
+
+    train_df.to_csv(base_dir + '/dataset/dataset1/trainset/train_base2.csv', index=False)
+    test_df.to_csv(base_dir + '/dataset/dataset1/testset/test_a_base2.csv', index=False)
+
 
 # %%
+process_base_onehot(os.getcwd(), dim=50)
+
+
+# %%
+def process_base_user(base_dir):
+    train_df = pd.read_csv(base_dir + '/dataset/dataset1/trainset/train_base2.csv')
+    test_df = pd.read_csv(base_dir + '/dataset/dataset1/testset/test_a_base2.csv')
+    for i in range(len(train_df)):
+        train_df.loc[i, 'user'] = train_df.loc[i, 'user'][6:]
+    for i in range(len(test_df)):
+        test_df.loc[i, 'user'] = test_df.loc[i, 'user'][6:]
+    train_df['user'] = pd.to_numeric(train_df['user'])
+    test_df['user'] = pd.to_numeric(test_df['user'])
+    train_df.to_csv(base_dir + '/dataset/dataset1/trainset/train_base2.csv', index=False)
+    test_df.to_csv(base_dir + '/dataset/dataset1/testset/test_a_base2.csv', index=False)
+
+
+# %%
+process_base_user(os.getcwd())
+
+# %%
+train_df = pd.read_csv(base_dir + '/dataset/dataset1/trainset/train_base2.csv')
+test_df = pd.read_csv(base_dir + '/dataset/dataset1/testset/test_a_base2.csv')
+train_df['user'] = pd.to_numeric(train_df['user'])
+test_df['user'] = pd.to_numeric(test_df['user'])
+train_df.to_csv(base_dir + '/dataset/dataset1/trainset/train_base2.csv', index=False)
+test_df.to_csv(base_dir + '/dataset/dataset1/testset/test_a_base2.csv', index=False)
+
+# %%
+train_df = pd.read_csv(base_dir + '/dataset/dataset1/trainset/train_base2.csv')
+print(train_df.info())
