b0b65ce7b5ff0496cabb0d3550ef32764aea3e4c
RickyL-2000 <15305813731@163.com>
2020-07-29 13:31:02 +0800

    把test修改成了auc，并加入了交叉验证。有维度错误

--- models/logistic_regression/lr_1_1.py
+++ models/logistic_regression/lr_1_1.py
@@ -8,16 +8,18 @@ from sklearn.metrics import roc_auc_score
 
 # %%
 base_dir = os.getcwd()
-x_df = pd.read_csv(base_dir + '/dataset/dataset1/trainset/train_base2.csv')
+x_df = pd.read_csv(base_dir + '/dataset/dataset1/trainset/train_base.csv')
 y_df = pd.read_csv(base_dir + '/dataset/raw_dataset/trainset/train_label.csv')
 data_x = np.array(x_df)
 # train_x = np.delete(train_x, 0, axis=1)
 data_y = np.array(y_df)
 
 # %%
-# 将x与y对应
+# 将x与y对应，并做预处理
 data_x = data_x[data_x[:, 0].argsort()]
 data_y = data_y[data_y[:, 0].argsort()]
+data_x = data_x[:, 1:].astype(float)
+data_y = data_y[:, 1:].astype(float).reshape(1, -1)[0]
 
 # %%
 # 归一化
@@ -33,17 +35,6 @@ state = np.random.get_state()
 np.random.shuffle(data_x)
 np.random.set_state(state)
 np.random.shuffle(data_y)
-
-# %%
-# 抽取验证集
-train_x = data_x[:int(0.9 * n), 1:]
-train_y = data_y[:int(0.9 * n), 1:]
-val_x = data_x[int(0.9 * n):, 1:]
-val_y = data_y[int(0.9 * n):, 1:]
-train_x = train_x.astype(float)
-train_y = train_y.astype(float).reshape(1, -1)[0]
-val_x = val_x.astype(float)
-val_y = val_y.astype(float).reshape(1, -1)[0]
 
 
 # %%
@@ -64,38 +55,53 @@ def test(pred, lab):
     return torch.mean(t.float())
 
 
+def get_score(pred, lab):
+    pred = nn.Softmax(pred).detach().numpy()
+    lab = lab.detach().numpy()
+    return roc_auc_score(pred[:, 1], lab)
+
+
 # %%
 net = LR()
 criterion = nn.CrossEntropyLoss()
 optm = torch.optim.SGD(net.parameters(), lr=1e-3, momentum=0.9)
 epochs = 2000
+group_n = 10
+group_size = epochs // group_n
 
 # %%
 # train
-for i in range(epochs):
-    net.train()
-    x = torch.from_numpy(train_x).float()
-    y = torch.from_numpy(train_y).long()
-    y_hat = net(x)
-    loss = criterion(y_hat, y)
-    optm.zero_grad()
-    loss.backward()
-    optm.step()
-    if (i+1) % 100 == 0:
-        net.eval()
-        val_in = torch.from_numpy(val_x).float()
-        val_lab = torch.from_numpy(val_y).long()
-        val_out = net(val_in)
-        accur = test(val_out, val_lab)
-        print("Epoch:{},Loss:{:.4f},Accuracy：{:.2f}".format(i + 1, loss.item(), accur))
+for j in range(group_n):
+    # 交叉验证
+    train_x = np.append(data_x[:j*group_size, :], data_x[(j+1)*group_size:, :])
+    train_y = np.append(data_y[:j*group_size], data_y[(j+1)*group_size:])
+    val_x = data_x[j*group_size: (j+1)*group_size, :]
+    val_y = data_y[j*group_size: (j+1)*group_size]
+    for i in range(j * group_size, (j+1) * group_size):
+        net.train()
+        x = torch.from_numpy(train_x).float()
+        y = torch.from_numpy(train_y).long()
+        y_hat = net(x)
+        loss = criterion(y_hat, y)
+        optm.zero_grad()
+        loss.backward()
+        optm.step()
+        if (i+1) % 100 == 0:
+            net.eval()
+            val_in = torch.from_numpy(val_x).float()
+            val_lab = torch.from_numpy(val_y).long()
+            val_out = net(val_in)
+            score = get_score(val_out, val_lab)
+            print("Epoch:{}, Loss:{:.4f}, Score：{:.2f}".format(i + 1, loss.item(), score))
 
 # %%
 # 测试集
-test_x = np.array(pd.read_csv(base_dir + '/dataset/dataset1/testset/test_a_base2.csv'))
+test_x = np.array(pd.read_csv(base_dir + '/dataset/dataset1/testset/test_a_base.csv'))
 y_df = pd.read_csv(base_dir + '/dataset/raw_dataset/testset/submit_example.csv')
 
 # %%
 test_x = test_x[test_x[:, 0].argsort()]
+test_x = test_x[:, 1:].astype(float)
 n_t, l_t = test_x.shape
 for j in range(l_t):
     meanVal = np.mean(test_x[:, j])
@@ -105,6 +111,9 @@ for j in range(l_t):
 # %%
 test_in = torch.from_numpy(test_x).float()
 test_out = net(test_in)
+pred = nn.Softmax(test_out).detach().numpy()[:, 1]
+y_df.loc[:, 'prob'] = pred
+y_df.to_csv(base_dir + '/models/logistic_regression/output_1_1.csv')
 
 
 # %%
