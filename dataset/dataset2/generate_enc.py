c82c544399569472ed88c18bf7b1bbb8029485ed
RickyL-2000 <15305813731@163.com>
2020-07-30 17:10:41 +0800

    初始化载入encoder和表头

--- dataset/dataset2/generate_enc.py
+++ dataset/dataset2/generate_enc.py
@@ -4,7 +4,6 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.decomposition import PCA
-from sklearn.externals import joblib
 base_dir = os.getcwd()
 
 # %%
@@ -29,6 +28,9 @@ pca_op_type.fit(onehot_op_type)
 result_op_type = pca_op_type.transform(onehot_op_type)
 mp_op_type = dict(zip(values_op_type_org, [code for code in result_op_type]))
 
+pd.DataFrame.from_dict(data=mp_op_type, orient='columns')\
+    .to_csv(base_dir + '/dataset/dataset2/encoders/enc_op_type.csv')
+
 # %%
 # op_mode onehot+pca
 op_mode = pd.concat([train_op_df['op_mode'], test_op_df['op_mode']])
@@ -45,6 +47,9 @@ pca_op_mode.fit(onehot_op_mode)
 result_op_mode = pca_op_mode.transform(onehot_op_mode)
 mp_op_mode = dict(zip(values_op_mode_org, [code for code in result_op_mode]))
 
+pd.DataFrame.from_dict(data=mp_op_mode, orient='columns')\
+    .to_csv(base_dir + '/dataset/dataset2/encoders/enc_op_mode.csv')
+
 # %%
 # net_type onehot
 net_type = pd.concat([train_op_df['net_type'], test_op_df['net_type']])
@@ -55,6 +60,9 @@ enc_net_type = OneHotEncoder()
 enc_net_type.fit(values_net_type)
 onehot_net_type = enc_net_type.transform(values_net_type)
 mp_op_net_type = dict(zip(values_net_type_org, [code for code in onehot_net_type]))
+
+pd.DataFrame.from_dict(data=mp_op_net_type, orient='columns')\
+    .to_csv(base_dir + '/dataset/dataset2/encoders/enc_op_net_type.csv')
 
 # %%
 # op_channel onehot+pca
@@ -72,6 +80,9 @@ pca_channel.fit(onehot_channel)
 result_channel = pca_channel.transform(onehot_channel)
 mp_op_channel = dict(zip(values_channel_org, [code for code in result_channel]))
 
+pd.DataFrame.from_dict(data=mp_op_channel, orient='columns')\
+    .to_csv(base_dir + '/dataset/dataset2/encoders/enc_op_channel.csv')
+
 # %%
 # tran_platform onehot
 platform = pd.concat([train_trans_df['platform'], test_trans_df['platform']])
@@ -82,6 +93,9 @@ enc_platform = OneHotEncoder()
 enc_platform.fit(values_platform)
 onehot_platform = enc_platform.transform(values_platform)
 mp_trans_platform = dict(zip(values_platform_org, [code for code in onehot_platform]))
+
+pd.DataFrame.from_dict(data=mp_trans_platform, orient='columns')\
+    .to_csv(base_dir + '/dataset/dataset2/encoders/enc_trans_platform.csv')
 
 # %%
 # tunnel_in onehot
@@ -94,6 +108,9 @@ enc_tunnel_in.fit(values_tunnel_in)
 onehot_tunnel_in = enc_tunnel_in.transform(values_tunnel_in)
 mp_trans_tunnel_in = dict(zip(values_tunnel_in_org, [code for code in onehot_tunnel_in]))
 
+pd.DataFrame.from_dict(data=mp_trans_tunnel_in, orient='columns')\
+    .to_csv(base_dir + '/dataset/dataset2/encoders/enc_trans_tunnel_in.csv')
+
 # %%
 # tunnel_out onehot
 tunnel_out = pd.concat([train_trans_df['tunnel_out'], test_trans_df['tunnel_out']])
@@ -104,6 +121,9 @@ enc_tunnel_out = OneHotEncoder()
 enc_tunnel_out.fit(values_tunnel_out)
 onehot_tunnel_out = enc_tunnel_out.transform(values_tunnel_out)
 mp_trans_tunnel_out = dict(zip(values_tunnel_out_org, [code for code in onehot_tunnel_out]))
+
+pd.DataFrame.from_dict(data=mp_trans_tunnel_out, orient='columns')\
+    .to_csv(base_dir + '/dataset/dataset2/encoders/enc_trans_tunnel_out.csv')
 
 # %%
 # trans_type1 onehot+pca
@@ -121,6 +141,9 @@ pca_type1.fit(onehot_type1)
 result_type1 = pca_type1.transform(onehot_type1)
 mp_trans_type1 = dict(zip(values_type1_org, [code for code in result_type1]))
 
+pd.DataFrame.from_dict(data=mp_trans_type1, orient='columns')\
+    .to_csv(base_dir + '/dataset/dataset2/encoders/enc_trans_type1.csv')
+
 # %%
 type2 = pd.concat([train_trans_df['type2'], test_trans_df['type2']])
 dim_type2 = 5
@@ -135,3 +158,6 @@ pca_type2 = PCA(n_components=dim_type2)
 pca_type2.fit(onehot_type2)
 result_type2 = pca_type2.transform(onehot_type2)
 mp_trans_type2 = dict(zip(values_type2_org, [code for code in result_type2]))
+
+pd.DataFrame.from_dict(data=mp_trans_type2, orient='columns')\
+    .to_csv(base_dir + '/dataset/dataset2/encoders/enc_trans_type2.csv')
--- dataset/dataset2/generate_main.py
+++ dataset/dataset2/generate_main.py
@@ -7,10 +7,32 @@ from sklearn.decomposition import PCA
 base_dir = os.getcwd()
 
 # %%
-feature_train_trans = pd.DataFrame()
-feature_test_trans = pd.DataFrame()
-feature_train_op = pd.DataFrame()
-feature_test_op = pd.DataFrame()
+# 初始化表头
+op_header = ['op_type_0', 'op_type_1', 'op_type_2', 'op_type_3', 'op_type_4', 'op_type_5', 'op_type_6', 'op_type_7',
+             'op_type_8', 'op_type_9', 'op_type_perc', 'op_type_std', 'op_type_n', 'op_mode_0', 'op_mode_1',
+             'op_mode_2', 'op_mode_3', 'op_mode_4', 'op_mode_5', 'op_mode_6', 'op_mode_7', 'op_mode_8', 'op_mode_9',
+             'op_mode_perc', 'op_mode_std', 'op_mode_n', 'op_device_perc', 'op_device_std', 'op_device_nan_perc',
+             'op_device_n', 'op_ip_perc', 'op_ip_std', 'op_ip_nan_perc', 'op_ip_n', 'op_net_type_0', 'op_net_type_1',
+             'op_net_type_2', 'op_net_type_3', 'op_net_type_perc', 'op_net_type_std', 'op_net_type_nan_perc',
+             'op_channel_0', 'op_channel_1', 'op_channel_2', 'op_channel_3', 'op_channel_4', 'op_channel_perc',
+             'op_channel_std', 'op_channel_n', 'op_ip_3_perc', 'op_ip_3_std', 'op_ip_3_nan_perc', 'op_ip_3_n',
+             'op_freq', 'op_ip_freq', 'op_ip_3_freq', 'op_ip_3_ch_freq', 'op_ip_48h_n', 'op_device_48h_n', 'op_48h_n']
+trans_header = ['trans_platform_0', 'trans_platform_1', 'trans_platform_2', 'trans_platform_3',
+                'trans_platform_4', 'trans_platform_5', 'trans_platform_perc', 'trans_platform_std', 'trans_platform_n',
+                'trans_tunnel_in_0', 'trans_tunnel_in_1', 'trans_tunnel_in_2', 'trans_tunnel_in_3', 'trans_tunnel_in_4',
+                'trans_tunnel_in_5', 'trans_tunnel_in_perc', 'trans_tunnel_in_std', 'trans_tunnel_in_n',
+                'trans_tunnel_in_nan_perc', 'trans_tunnel_out_0', 'trans_tunnel_out_1', 'trans_tunnel_out_2',
+                'trans_tunnel_out_3', 'trans_tunnel_out_perc', 'trans_tunnel_out_std', 'trans_tunnel_n',
+                'trans_amount_max', 'trans_amount_avg', 'trans_amount_std', 'trans_type1_0', 'trans_type1_1',
+                'trans_type1_2', 'trans_type1_3', 'trans_type1_4', 'trans_type1_perc', 'trans_type1_std',
+                'trans_ip_perc', 'trans_ip_std', 'trans_ip_nan_perc', 'trans_ip_n']
+
+
+# %%
+feature_train_trans = pd.DataFrame(columns=trans_header)
+feature_test_trans = pd.DataFrame(columns=trans_header)
+feature_train_op = pd.DataFrame(columns=op_header)
+feature_test_op = pd.DataFrame(columns=op_header)
 train_base_df = pd.read_csv(base_dir + '/dataset/dataset2/trainset/train_base.csv')
 train_op_df = pd.read_csv(base_dir + '/dataset/dataset2/trainset/train_op.csv')
 train_trans_df = pd.read_csv(base_dir + '/dataset/dataset2/trainset/train_trans.csv')
@@ -21,57 +43,63 @@ train_n = len(train_base_df)
 test_n = len(test_base_df)
 
 # %%
-# TODO: 要先把encoder计算好
+# load encoder
+op_type = np.loadtxt(base_dir + '/dataset/dataset2/encoders/enc_op_type.csv', delimiter=',')
+mp_op_type = {}
+for i in range(op_type.shape[0]):
+    mp_op_type[op_type[i, 0]] = op_type[i, 1:].tolist()
 
-# %%
-# op_type onehot+pca
-op_type = pd.concat([train_op_df['op_type'], test_op_df['op_type']])
-dim_op_type = 10
+op_mode = np.loadtxt(base_dir + '/dataset/dataset2/encoders/enc_op_mode.csv', delimiter=',')
+mp_op_mode = {}
+for i in range(op_mode.shape[0]):
+    mp_op_mode[op_mode[i, 0]] = op_mode[i, 1:].tolist()
 
-values_op_type_org = op_type.unique().tolist()      # 原来shape的values
-values_op_type = np.array(values_op_type_org).reshape(len(values_op_type_org), -1)
-enc_op_type = OneHotEncoder()
-enc_op_type.fit(values_op_type)
-onehot_op_type = enc_op_type.transform(values_op_type).toarray()
+net_type = np.loadtxt(base_dir + '/dataset/dataset2/encoders/enc_net_type.csv', delimiter=',')
+mp_net_type = {}
+for i in range(net_type.shape[0]):
+    mp_net_type[net_type[i, 0]] = net_type[i, 1:].tolist()
 
-pca_op_type = PCA(n_components=dim_op_type)
-pca_op_type.fit(onehot_op_type)
-result_op_type = pca_op_type.transform(onehot_op_type)
-mp_op_type = dict(zip(values_op_type_org, [code for code in result_op_type]))
+channel = np.loadtxt(base_dir + '/dataset/dataset2/encoders/enc_channel.csv', delimiter=',')
+mp_channel = {}
+for i in range(channel.shape[0]):
+    mp_channel[channel[i, 0]] = channel[i, 1:].tolist()
 
-# %%
-# op_mode onehot+pca
-op_mode = pd.concat([train_op_df['op_mode'], test_op_df['op_mode']])
-dim_op_mode = 10
+platform = np.loadtxt(base_dir + '/dataset/dataset2/encoders/enc_platform.csv', delimiter=',')
+mp_platform = {}
+for i in range(platform.shape[0]):
+    mp_platform[platform[i, 0]] = platform[i, 1:].tolist()
 
-values_op_mode_org = op_mode.unique().tolist()      # 原来shape的values
-values_op_mode = np.array(values_op_mode_org).reshape(len(values_op_mode_org), -1)
-enc_op_mode = OneHotEncoder()
-enc_op_mode.fit(values_op_mode)
-onehot_op_mode = enc_op_mode.transform(values_op_mode).toarray()
+tunnel_in = np.loadtxt(base_dir + '/dataset/dataset2/encoders/enc_tunnel_in.csv', delimiter=',')
+mp_tunnel_in = {}
+for i in range(tunnel_in.shape[0]):
+    mp_tunnel_in[tunnel_in[i, 0]] = tunnel_in[i, 1:].tolist()
 
-pca_op_mode = PCA(n_components=dim_op_mode)
-pca_op_mode.fit(onehot_op_mode)
-result_op_mode = pca_op_mode.transform(onehot_op_mode)
-mp_op_mode = dict(zip(values_op_mode_org, [code for code in result_op_mode]))
+tunnel_out = np.loadtxt(base_dir + '/dataset/dataset2/encoders/enc_tunnel_out.csv', delimiter=',')
+mp_tunnel_out = {}
+for i in range(tunnel_out.shape[0]):
+    mp_tunnel_out[tunnel_out[i, 0]] = tunnel_out[i, 1:].tolist()
 
-# %%
-# net_type onehot
-net_type = pd.concat([train_op_df['net_type'], test_op_df['net_type']])
+type1 = np.loadtxt(base_dir + '/dataset/dataset2/encoders/enc_type1.csv', delimiter=',')
+mp_type1 = {}
+for i in range(type1.shape[0]):
+    mp_type1[type1[i, 0]] = type1[i, 1:].tolist()
 
-values_net_type_org = net_type.unique().tolist()
-values_net_type = np.array(values_net_type_org).reshape(len(values_net_type_org), -1)
-enc_net_type = OneHotEncoder()
-enc_net_type.fit(values_net_type)
-onehot_net_type = enc_net_type.transform(values_net_type)
-mp_net_type = dict(zip(values_net_type_org, [code for code in onehot_net_type]))
-
-# %%
+type2 = np.loadtxt(base_dir + '/dataset/dataset2/encoders/enc_type2.csv', delimiter=',')
+mp_type2 = {}
+for i in range(type2.shape[0]):
+    mp_type2[type2[i, 0]] = type2[i, 1:].tolist()
 
 
 # %%
 # train set
 for i in range(train_n):
-    tr_trans_user = train_trans_df[train_trans_df['user'] == train_base_df.loc[i, 'user']]
-    tr_op_user = train_op_df[train_op_df['user'] == train_base_df.loc[i, 'user']]
+    cur_user = train_base_df['user'].loc[i]
+    tr_trans_user = train_trans_df[train_trans_df['user'] == cur_user]
+    tr_op_user = train_op_df[train_op_df['user'] == cur_user]
+
+    tr_trans_user_n = len(tr_trans_user)    # 该用户的trans记录条数
+    tr_op_user_n = len(tr_op_user)          # 该用户的trans记录条数
+
+    # -----------op_type----------- #
+    op_type_df = pd.DataFrame(columns=['op_type_' + str(i) for i in range(10)])
 
