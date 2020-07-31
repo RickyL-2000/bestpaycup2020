3efa9d9cda22afd8e3ed5305abdd960796caed18
RickyL-2000 <15305813731@163.com>
2020-07-31 10:46:29 +0800

    对trans的并入预处理

--- dataset/dataset2/trans_proc.py
+++ dataset/dataset2/trans_proc.py
@@ -7,51 +7,72 @@ base_dir = os.getcwd()
 
 
 # %%
-def trans_proc(df, isTrain=True):
-    def user2int(entry):
-        level = re.search("^(Train_ /TestA_)([0-9]+)", entry)
-        if level:
-            return int(level.group(2))
-        return entry
+def user2int(entry):
+    level = re.search("^(Train_ /TestA_)([0-9]+)", entry)
+    if level:
+        return int(level.group(2))
+    return entry
 
-    def tm2int(entry):
-        days = int(entry[:entry.find('days')])
-        flag = org.find(':')
-        hours = int(entry[entry.find('s') + 2: flag])
-        minutes = int(entry[flag + 1: entry.find(':', flag + 1)])
-        seconds = float(entry[entry.find(':', flag + 1) + 1:])
-        return ((24 * days + hours) * 60 + minutes) * 60 + seconds
 
-    n = len(df)
-    for i in range(n):
-        df.loc[i, 'user'] = df.loc[i, 'user'][6:]
-    df = df.fillna({'tunnel_in': 'tunnel_in_nan'}, inplace=True)
-    df['tunnel_out'].fillna(df['tunnel_out'].mode()[0], inplace=True)
-    df['type2'].fillna('type2_nan', inplace=True)
-    for i in range(n):
-        org = df.loc[i, 'tm_diff']
-        days = int(org[:org.find('days')])
-        flag = org.find(':')
-        hours = int(org[org.find('s') + 2: flag])
-        minutes = int(org[flag + 1: org.find(':', flag + 1)])
-        seconds = float(org[org.find(':', flag + 1) + 1:])
-        df.loc[i, 'tm_diff'] = ((24 * days + hours) * 60 + minutes) * 60 + seconds
-
-    if isTrain:
-        df.to_csv(base_dir + '/dataset/dataset2/trainset/train_trans.csv', index=False)
-    else:
-        df.to_csv(base_dir + '/dataset/dataset2/testset/test_a_trans.csv', index=False)
+def tm2int(entry):
+    days = int(entry[:entry.find('days')])
+    flag = org.find(':')
+    hours = int(entry[entry.find('s') + 2: flag])
+    minutes = int(entry[flag + 1: entry.find(':', flag + 1)])
+    seconds = float(entry[entry.find(':', flag + 1) + 1:])
+    return ((24 * days + hours) * 60 + minutes) * 60 + seconds
 
 
 # %%
-if __name__ == '__main__':
-    train_df = pd.read_csv(base_dir + r'/dataset/raw_dataset/trainset/train_trans.csv')
-    test_df = pd.read_csv(base_dir + r'/dataset/raw_dataset/testset/test_a_trans.csv')
-    trans_proc(train_df, isTrain=True)
-    trans_proc(test_df, isTrain=False)
-
-# %%
-'''
 train_df = pd.read_csv(base_dir + r'/dataset/raw_dataset/trainset/train_trans.csv')
-print(train_df['tunnel_out'])
-'''
+n = len(train_df)
+for i in range(n):
+    if i % 1000 == 0:
+        print(i)
+    train_df['user'].loc[i] = train_df['user'].loc[i][6:]
+train_df = train_df.fillna({'tunnel_in': 'tunnel_in_nan'})
+t_out_mode = train_df['tunnel_out'].mode()[0]
+train_df = train_df.fillna({'tunnel_out': t_out_mode})
+train_df = train_df.fillna({'type2': 'type2_nan'})
+train_df = train_df.fillna({'ip': 'ip_nan'})
+train_df = train_df.fillna({'ip_3': 'ip_3_nan'})
+for i in range(n):
+    if i % 1000 == 0:
+        print(i)
+    org = train_df.loc[i, 'tm_diff']
+    days = int(org[:org.find('days')])
+    flag = org.find(':')
+    hours = int(org[org.find('s') + 2: flag])
+    minutes = int(org[flag + 1: org.find(':', flag + 1)])
+    seconds = float(org[org.find(':', flag + 1) + 1:])
+    train_df['tm_diff'].loc[i] = ((24 * days + hours) * 60 + minutes) * 60 + seconds
+
+train_df.to_csv(base_dir + '/dataset/dataset2/trainset/train_trans.csv', index=False)
+
+# %%
+test_df = pd.read_csv(base_dir + r'/dataset/raw_dataset/testset/test_a_trans.csv')
+n = len(test_df)
+for i in range(n):
+    if i % 1000 == 0:
+        print(i)
+    test_df['user'].loc[i] = test_df['user'].loc[i][6:]
+test_df = test_df.fillna({'tunnel_in': 'tunnel_in_nan'})
+t_out_mode = test_df['tunnel_out'].mode()[0]
+test_df = test_df.fillna({'tunnel_out': t_out_mode})
+test_df = test_df.fillna({'type2': 'type2_nan'})
+test_df = test_df.fillna({'ip': 'ip_nan'})
+test_df = test_df.fillna({'ip_3': 'ip_3_nan'})
+for i in range(n):
+    if i % 1000 == 0:
+        print(i)
+    org = test_df.loc[i, 'tm_diff']
+    days = int(org[:org.find('days')])
+    flag = org.find(':')
+    hours = int(org[org.find('s') + 2: flag])
+    minutes = int(org[flag + 1: org.find(':', flag + 1)])
+    seconds = float(org[org.find(':', flag + 1) + 1:])
+    test_df['tm_diff'].loc[i] = ((24 * days + hours) * 60 + minutes) * 60 + seconds
+
+test_df.to_csv(base_dir + '/dataset/dataset2/testset/test_a_trans.csv', index=False)
+
+
