3191a243fdd895b14eb069348828c066c8a24275
RickyL-2000 <ruiqi.18@intl.zju.edu.cn>
2020-09-02 22:43:46 +0800

    比赛结束，洗洗睡吧

--- models/treemodel/xgb_4_1.py
+++ models/treemodel/xgb_4_1.py
@@ -1,5 +1,6 @@
 # %%
 import xgboost as xgb
+from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import roc_auc_score
@@ -32,7 +33,7 @@ X_train, X_val, y_train, y_val = train_test_split(data_x, data_y, test_size=0.1)
 train_data = xgb.DMatrix(X_train, label=y_train)
 val_data = xgb.DMatrix(X_val, label=y_val)
 param = {
-    'silent': 1,
+    'silent': 0,
     'max_depth': 10,
     'n_estimators': 50,
     'learning_rate': 0.1,
@@ -45,3 +46,25 @@ model = xgb.train(param, train_data, num_boost_round=100)
 y_hat = model.predict(val_data)
 
 print('auc: ', roc_auc_score(y_val, y_hat))
+
+# %%
+clf = XGBClassifier(
+    learning_rate=0.1,
+    n_estimators=1000,
+    max_depth=5,
+    min_child_weight=1,
+    gamma=0,
+    subsample=0.8,
+    colsample_bytree=0.8,
+    objective='binary:logistic',
+    nthread=4,
+    scale_pos_weight=1,
+    seed=27,
+    verbosity=1
+)
+clf.fit(X_train, y_train)
+
+# %%
+y_pre = clf.predict(X_val)
+y_pro = clf.predict_proba(X_val)[:, 1]
+print('auc: ', roc_auc_score(y_val, y_pro))
